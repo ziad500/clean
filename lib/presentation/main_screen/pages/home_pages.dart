e2e48e91d4e78a7d66e19b39f9c69f451e4f5f71
Your Name <you@example.com>
2022-10-18 00:03:43 +0200

    #finish register

---
+++ lib/presentation/main_screen/pages/home_pages.dart
@@ -0,0 +1,20 @@
+import 'package:clean/presentation/resources/strings_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(AppStrings.home),
+    );
+  }
+}
---
+++ lib/presentation/main_screen/pages/settings_page.dart
@@ -0,0 +1,20 @@
+import 'package:clean/presentation/resources/strings_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(AppStrings.settings),
+    );
+  }
+}
